b8be47477a5b123d43b2a925c19f9ce3a3b1eb85
akw83 <lokytheone@gmail.com>
2019-01-26 22:47:33 +0100

    added algo move train

--- src/app/disposition/disposition.component.ts
+++ src/app/disposition/disposition.component.ts
@@ -14,14 +14,17 @@ export class DispositionComponent implements OnInit {
   buttonModel = 'stop';
   timer: any;
   trainDetail: Train;
+  counter: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.counter = 0;
     this.createTrainDataSet();
     this.createTrainDataSVG(this.trainData);
   }
 
+  /* controlls ---------------------------------------------------------- */
   private createTrainDataSet(): Train[] {
     return this.trainData = TRAINS;
   }
@@ -46,6 +49,8 @@ export class DispositionComponent implements OnInit {
     this.trainDetail = train;
     console.log('created details: ' + this.trainDetail.name);
   }
+
+  /* TODO move all function below into a service! */
 
   // transform 1 set of train data -----------------------------
   private createPoint(name: string, coord: number[][]): {name: string, coord: string, lastCoordinate: string[]} {
@@ -83,27 +88,56 @@ export class DispositionComponent implements OnInit {
   private simulateTime(trains: Train[]): Train[] {
     const data = [];
     for (let i = 0; i < trains.length; i++) {
-      data.push(this.simulatedTimeSingleTrain(trains[i]));
+      data.push(this.simulateSingleTrain(trains[i]));
     }
     return data;
   }
 
-  private simulatedTimeSingleTrain(train: Train): Train {
+  /* simulate train behavior */
+  private simulateSingleTrain(train: Train): Train {
     const updatedData: Train = train;
+    this.counter += 1;
+    if (this.counter / 100 > 1) {
+      updatedData.coordinates.push(this.moveTrain(train.coordinates));
+      this.counter = 0;
+    }
     for (let i = 0; i < train.coordinates.length; i++) {
       updatedData.coordinates[i][1] += 0.1;
+      /* coord out of viewbox --> remove coord from array */
       if (train.coordinates[i][1] >= 900) {
-        console.log('b4: ' + updatedData);
         updatedData.coordinates.splice(i, 1);
-        console.log('after: ' + updatedData);
       }
     }
-    if (updatedData.coordinates.length === 0) {
-      updatedData.coordinates.push(this.addNewTrain());
+    /* begin train with a fresh new coord after all coordinates are out of bound */
+    if (updatedData.coordinates.length === 0 ||
+      updatedData.coordinates[updatedData.coordinates.length - 1][0] < 0 ||
+      updatedData.coordinates[updatedData.coordinates.length - 1][1] > 800) {
+      updatedData.coordinates.push(this.addNewTrainCoord());
     }
     return updatedData;
   }
 
+  private moveTrain(trainCoordinate) {
+    const leftDirection = this.simulateTrainMovement(
+      0, trainCoordinate[trainCoordinate.length - 1][0], trainCoordinate[trainCoordinate.length - 1][1]);
+    const rightDirection = this.simulateTrainMovement(
+      1, trainCoordinate[trainCoordinate.length - 1][0], trainCoordinate[trainCoordinate.length - 1][1]);
+
+    if (trainCoordinate.length > 1) {
+      if ((trainCoordinate[trainCoordinate.length - 1][0] - trainCoordinate[trainCoordinate.length - 2][0]) >= 0) {
+         return leftDirection;
+      } else {
+        return rightDirection;
+      }
+    }
+    if (trainCoordinate[0][0] > 400) {
+      return rightDirection;
+    } else {
+      return leftDirection;
+    }
+  }
+
+  /* simulate time flow behavior */
   private startTimeFlow(simulationSpeed: number): void {
     /* let simulatedTrains: Train[] = this.trainData; */
     this.timer = setInterval(() => {
@@ -112,11 +146,28 @@ export class DispositionComponent implements OnInit {
     }, 30 / simulationSpeed);
   }
 
-  private addNewTrain(): [number, number] {
+  private simulateTrainMovement(direction: 0|1, lastXValue, lastYValue): [number, number] {
+    let x: number;
+    if (direction === 0) {
+      x = lastXValue + Math.floor(Math.random() * 20);
+    } else {
+      x = lastXValue - Math.floor(Math.random() * 20);
+    }
+    const y = lastYValue - Math.floor(Math.random() * 20);
+    return [x, y];
+  }
+
+  /* begin new train with start coord */
+  private addNewTrainCoord(): [number, number] {
     const direction = Math.round(Math.random());
     const x = (direction === 0) ? 0 : 660;
     const y = Math.floor((Math.random() * 400) + 50);
     return [x, y];
+  }
+
+  /* add new train from view */
+  public addNewTrainOnView(event: any) {
+    return event;
   }
 
 }
